e16695a9fdb25d0db3e6964a3fbe57c298c69b14
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-16 18:13:17 +0600

    HW#86 Added Track History

---
+++ app/trackHistory.js
@@ -0,0 +1,52 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const Track = require("../models/Tracks");
+const TrackHistory = require("../models/TrackHistory");
+
+router.get("/", async (req, res) => {
+    const token = req.get("Authorization");
+    if (!token) {
+        return res.status(401).send({ error: "No token presented" });
+    };
+    const user = await User.findOne({ token });
+    if (!user) {
+        return res.status(401).send({ error: "Wrong token" });
+    };
+    try {
+        const trackHistory = await TrackHistory.find({user: user});
+        res.send(trackHistory);
+    }catch(e){
+        res.status(400).send(e);
+    };
+});
+
+router.post("/", async (req, res) => {
+    const token = req.get("Authorization");
+    if (!token) {
+        return res.status(401).send({ error: "No token presented" });
+    };
+    const user = await User.findOne({ token });
+    if (!user) {
+        return res.status(401).send({ error: "Wrong token" });
+    };
+    const trackID = req.body.track;
+    const track = await Track.findById(trackID);
+    if (!track) {
+        return res.status(400).send({ error: 'Track does not exist' });
+    } else {
+        const trackHistory = new TrackHistory({
+            track: trackID,
+            user: user._id,
+            dateTime: new Date().toISOString(),
+        });
+        try {
+            await trackHistory.save();
+            res.send(trackHistory);
+        } catch (e) {
+            res.status(400).send(e);
+        };
+    };
+});
+
+
+module.exports = router;
