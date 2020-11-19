baeae45436e9a8f657ccb5c5e0307fb50f001fb0
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-11-19 13:44:31 +0600

    Added config

--- index.js
+++ index.js
@@ -8,13 +8,14 @@ const users = require("./app/users");
 const trackHistory = require("./app/trackHistory");
 const app = express();
 const port = 8000;
+const config = require("./config");
 
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
 const run = async () => {
-    await mongoose.connect("mongodb://localhost/music-app", { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+    await mongoose.connect(config.db.url + "/" + config.db.name,  { useNewUrlParser: true , useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
 
     app.use("/artists", artists());
     app.use("/albums", album());
