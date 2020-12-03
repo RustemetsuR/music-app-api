6142eae75b499f14fed16bafb03210ed5fc93ef4
Rustem Ibraimov <ibraimov.rustem2003@gmail.com>
2020-12-03 16:32:35 +0600

    Added fb

--- config.js
+++ config.js
@@ -8,6 +8,9 @@ module.exports = {
   db: {
     name: 'music-app',
     url: 'mongodb://localhost',
-
+  },
+  fb: {
+    appId: "310932736689445",
+    appSecret: "ba779bdca7d271202078a94b9b92e485"
   }
 };
